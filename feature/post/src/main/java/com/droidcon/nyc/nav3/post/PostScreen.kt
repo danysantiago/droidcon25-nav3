eb09014b1614847bea102ded393b15a592539534
jbwoods <jbwoods@google.com>
2025-06-24 14:45:48 -0400

    Add padding to Feed Screen
    Add weight to PostScreen to move the favorites down.

--- feature/post/src/main/java/com/droidcon/nyc/nav3/post/PostScreen.kt
+++ feature/post/src/main/java/com/droidcon/nyc/nav3/post/PostScreen.kt
@@ -39,7 +39,7 @@ internal fun PostScreen(backstack: TopLevelBackStack<NavKey>, post: Post) {
             Image(painterResource(cat.imageId), cat.text, imageModifier)
         }
         Text(cat.text, modifier = Modifier.padding(10.dp))
-        Spacer(Modifier.width(20.dp))
+        Spacer(Modifier.weight(1f))
         Button(
             onClick = { cat.favorite.value = !cat.favorite.value },
             colors = ButtonDefaults.buttonColors(containerColor = Color.Companion.Blue),
