83072b3620c84c80c1499b8ce65eaacd06857b6f
jbwoods <jbwoods@google.com>
2025-06-24 05:27:55 -0400

    Pre-add imports for the first implementation

--- app/src/main/java/com/droidcon/nyc/nav3/MainActivity.kt
+++ app/src/main/java/com/droidcon/nyc/nav3/MainActivity.kt
@@ -7,7 +7,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entry
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.ui.NavDisplay
+import com.droidcon.nyc.nav3.feed.FeedScreen
+import com.droidcon.nyc.nav3.post.Post
+import com.droidcon.nyc.nav3.post.PostScreen
 import com.droidcon.nyc.nav3.ui.theme.NycDroidConTheme
 
 class MainActivity : ComponentActivity() {
